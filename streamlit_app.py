4957034b1a5255d389af12ea31e9de12cf805a43
husni.habil <husni.habil@grabtaxi.com>
2024-07-30 10:27:24 +0700

    Adding assets

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,12 @@ from get_response_spellvault import get_response_content_spellvault
 from get_response_azure import get_response_content_azure
 from css_style import get_css_style
 
+# Get avatar
+BOT_AVATAR = "./assets/avatar/grabmerchant.png"
+
+# Import style
 style = get_css_style()
+
 
 # Inject the CSS style
 st.markdown(style, unsafe_allow_html=True)
@@ -18,15 +23,19 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    if message["role"] == "assistant":
+        with st.chat_message(message["role"], avatar=BOT_AVATAR):
+            st.markdown(message["content"])
+    else:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    with st.chat_message("assistant"):
+    with st.chat_message("assistant", avatar=BOT_AVATAR):
         with st.spinner('Waiting for response...'):
             # response_content = get_response_content_spellvault(prompt)
             response_content = get_response_content_azure(prompt)
