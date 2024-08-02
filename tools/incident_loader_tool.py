46f820268765eae3be26ff7cdba303de37f0eaca
husni.habil <husni.habil@grabtaxi.com>
2024-08-02 15:15:58 +0700

    Update lates BE

--- tools/incident_loader_tool.py
+++ tools/incident_loader_tool.py
@@ -6,7 +6,7 @@ from langchain_core.tools import tool
 
 
 @tool
-def incident_info_loader_tool(paxID: int):
+def incident_info_loader_tool(incidentID: int):
     """Returns a result json"""
     current_dir = os.path.dirname(os.path.realpath(__file__))
     file_path = os.path.join(current_dir, "../mock_data/trainingdata_for_nbre.csv")
@@ -14,9 +14,9 @@ def incident_info_loader_tool(paxID: int):
     if not os.path.isfile(file_path):
         raise FileNotFoundError(f"File not found: {file_path}")
 
-    df = pd.read_csv(file_path,index_col="pax_id")
-    if paxID in df.index:
-        result_dict = df.loc[paxID].to_dict()
+    df = pd.read_csv(file_path,index_col="ticket_id")
+    if incidentID in df.index:
+        result_dict = df.loc[incidentID].to_dict()
 
         result_json = json.dumps(result_dict)
         return result_json
